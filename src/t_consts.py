888d16ed707005dd440b25ba6b8a08cfcfd13b77
Katherine R <katrussdev@protonmail.com>
2024-02-23 13:17:37 +0000

    feat: admin actions

--- src/t_consts.py
+++ src/t_consts.py
@@ -1,5 +1,5 @@
 """global-scope data for users and tasks"""
 
-USER_LIST = []
-TASK_LIST = []
+USER_LIST = []  # Global Users Information
+TASK_LIST = []  # Global Task Information
 CURRENT_USER = None  # place for the user currently using the app
---
+++ src/tests/objects/test_screen.py
@@ -0,0 +1,1 @@
+"""Screen Object Unit Testing"""
