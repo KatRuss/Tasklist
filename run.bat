138b4064c84ee243669a335d5edd7376abcb1742
Katherine R <katrussdev@protonmail.com>
2024-02-28 11:41:25 +0000

    batch scripts now use venv

--- run.bat
+++ run.bat
@@ -1,2 +1,9 @@
-pip install -r requirements.txt
+REM Activating Virtual Enviroment
+python -m venv venv
+CALL venv\Scripts\activate.bat
+
+REM Installing Python Dependencies
+python -m pip install -r requirements.txt
 python tasklist.py
+
+rmdir /s /q venv
--- run_tests.bat
+++ run_tests.bat
@@ -1,3 +1,10 @@
-pip install -r requirements.txt
-pip install pytest
+REM Activating Virtual Enviroment
+python -m venv venv
+CALL venv\Scripts\activate.bat
+
+REM Installing Python Dependencies
+python -m pip install -r requirements.txt
+python -m pip install pytest
 python -m pytest src/tests
+
+rmdir /s /q venv
